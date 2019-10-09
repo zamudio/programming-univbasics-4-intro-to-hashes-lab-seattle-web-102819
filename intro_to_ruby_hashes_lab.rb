2e3fb8cce56537fcb53a709b0945478165e6efb1
Michael Z <mziacm@gmail.com>
2019-10-09 08:16:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,12 +20,9 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  #
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
-  if hash[key]
-    hash[key] += 1
-  else
-    hash[key] = 1
+  #
 end
